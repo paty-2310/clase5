0cfb798a9c2dbb2e7a139e105c32b8f71adf5d97
paty-2310 <patriciamayora86@gmail.com>
2025-09-13 16:17:22 -0400

    Update adivina_el_numero.py

--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -8,14 +8,17 @@ while True :
     ingrese = int(input("ingrese el numero: ")) 
     if ingrese == 8 : 
         print("\nusted ha elegido el numero correcto\n") 
-    elif ingrese >= 1 or ingrese <= 5 : 
+    elif ingrese >= 1 and ingrese <= 5 : 
         print("estas muy abajo del numero")
-    elif ingrese > 6 or ingrese < 10 : 
-        print("estas muy cerca del numero") 
+    elif ingrese => 6 or ingrese <= 10 : 
+        print("estas muy cerca del numero")
+    elif ingrese > 10 :
+    print("debe elegir un numero en el rango del 1 al 10")
     else : 
-        print("debes elegir un numero en el rango drl 1 al 10")
+        print("debe ingresar un valor")
     
     
 
 
     
+
